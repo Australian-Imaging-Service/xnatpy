4416971184dc2498f6e3261882780176b8e11a7d
Hakim Achterberg <hakim.achterberg@gmail.com>
2015-09-08 14:57:25 +0200

    Fixed version number for pypi upload

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ import os
 from setuptools import setup
 
 # Get information about the version (polling mercurial if possible)
-version = '0.1.3'
+version = '0.1.0'
 dirstate = './.hg/dirstate'
 
 if __name__ == '__main__':
