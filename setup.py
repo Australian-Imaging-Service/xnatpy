263490b8b0896632f27b6af75b13984436866526
Hakim Achterberg <hakim.achterberg@gmail.com>
2018-12-06 09:37:59 +0100

    Add list of contributors
    --HG--
    branch : develop

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
             "Topic :: Scientific/Engineering :: Medical Science Apps.",
             ]
