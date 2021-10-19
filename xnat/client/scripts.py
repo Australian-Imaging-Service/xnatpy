3b08a596237db269d4b4eb577c1dc06e78d6f823
Ivan Bocharov <i.bocharov@erasmusmc.nl>
2021-10-19 19:11:48 +0200

    Added data integrity check script.

--- xnat/client/scripts.py
+++ xnat/client/scripts.py
@@ -1,8 +1,10 @@
 import click
 import xnat
 
-from ..scripts.copy_project import XNATProjectCopier
+from time import sleep
 
+from ..scripts.copy_project import XNATProjectCopier
+from ..scripts.data_integrity_check import XNATIntegrityCheck
 
 @click.group(name="script")
 def script():
@@ -25,3 +27,21 @@ def copy_project(source_host, source_project, dest_host, dest_project):
             # Create and start copier
             copier = XNATProjectCopier(source_xnat, source_project, dest_xnat, dest_project)
             copier.start()
+
+@script.command()
+@click.option("--host", required=True, help="XNAT URL")
+@click.option("--xnat-home-dir", required=True, help="Path to XNAT home directory")
+@click.option("--report", required=True, help="Path to report file")
+def data_integrity_check(host, xnat_home_dir, report): 
+    xnat_integrity_checker = XNATIntegrityCheck(host, xnat_home_dir)
+    xnat_integrity_checker.start()
+    print('progress\t FS\tXNAT')
+    while xnat_integrity_checker.is_running():
+        xnat_progress, fs_progress = xnat_integrity_checker.progress()
+        print(f'\t\t{fs_progress*100}\t{xnat_progress*100}', end='\r')
+        sleep(1)
+    fs_progress, xnat_progress = xnat_integrity_checker.progress()
+    print(f'\t\t{fs_progress*100}\t{xnat_progress*100}')
+    print("--- REPORT ---")
+    xnat_integrity_checker.write_report(report)
+    print('--- DONE ---')
