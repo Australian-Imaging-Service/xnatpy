7677ac3f92f4c0913a989325f4eedd2bb4b86a80
Ivan Bocharov <i.bocharov@erasmusmc.nl>
2021-11-10 18:26:31 +0100

    Added archiving commands

--- xnat/client/prearchive.py
+++ xnat/client/prearchive.py
@@ -1,5 +1,6 @@
 import collections
-from os import stat
+from os import O_WRONLY, stat
+from re import sub
 import click
 import xnat
 
@@ -57,3 +58,50 @@ def move(ctx, project, dest_project, label, subject, status):
         else:
             for sess in selected_sessions:
                 sess.move(dest_project)
+
+@prearchive.command()
+@click.argument('--sessionid')
+@click.argument('--project')
+@click.option('--subject')
+@click.option('--experiment')
+@click.option('--overwrite', is_flag=True)
+@click.option('--quarantine')
+@click.option('--trigger-pipelines', is_flag=True)
+@click.pass_context
+def archive(ctx, sessionid, project, subject, experiment, overwrite, quarantine, trigger_pipelines):
+    ctx = unpack_context(ctx)
+
+    with xnat.connect(ctx.host, user=ctx.user, netrc_file=ctx.netrc, jsession=ctx.jsession,
+                      cli=True, loglevel=ctx.loglevel) as session:
+        selected_session = None
+        for sess in session.prearchive.sessions:
+            if sess.id == sessionid:
+                selected_session = sess
+                break
+        
+        if not selected_session:
+            session.logger.warning("No prearchive sessions have been selected based on your criteria!")
+        else:
+            sess.archive(project=project, subject=subject, experiment=experiment, overwrite=overwrite, quarantine=quarantine, trigger_pipelines=trigger_pipelines)
+
+
+prearchive.command()
+@click.argument('--project', '-p')
+@click.option('--label', '-l')
+@click.option('--subject', '-s')
+@click.option('--status')
+@click.option('--overwrite', is_flag=True)
+@click.option('--quarantine')
+@click.option('--trigger-pipelines', is_flag=True)
+@click.pass_context
+def bulk_archive(ctx, project, dest_project, label, subject, status, overwrite, quarantine, trigger_pipelines):
+    ctx = unpack_context(ctx)
+
+    with xnat.connect(ctx.host, user=ctx.user, netrc_file=ctx.netrc, jsession=ctx.jsession,
+                      cli=True, loglevel=ctx.loglevel) as session:
+        selected_sessions = session.prearchive.find(project=project, subject=subject, label=label, status=status)
+        if not selected_sessions:
+            session.logger.warning("No prearchive sessions have been selected based on your criteria!")
+        else:
+            for sess in selected_sessions:
+                sess.archive(overwrite=overwrite, quarantine=quarantine, trigger_pipelines = trigger_pipelines)
