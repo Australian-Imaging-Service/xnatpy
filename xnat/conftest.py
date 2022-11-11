ad5c2b251b3ff207f5fdcb0c74a1f5254b560ca1
Hakim Achterberg <hakim.achterberg@gmail.com>
2022-11-11 22:12:58 +0100

    More work on tests

--- xnat/conftest.py
+++ xnat/conftest.py
@@ -1,5 +1,21 @@
+# Copyright 2011-2015 Biomedical Imaging Group Rotterdam, Departments of
+# Medical Informatics and Radiology, Erasmus MC, Rotterdam, The Netherlands
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 from typing import Any, Pattern, Union
+from unittest.mock import patch
 
 import requests
 
@@ -7,7 +23,16 @@ from pytest import fixture
 from pytest_mock import MockerFixture
 from requests_mock import Mocker
 
+from xnat.core import XNATObject
 from xnat.session import XNATSession
+
+
+class CreatedObject:
+    def __init__(self, uri, type_, fieldname, **kwargs):
+        self.uri = uri
+        self.type = type_
+        self.fieldname = fieldname
+        self.kwargs = kwargs
 
 
 class XnatpyRequestsMocker(Mocker):
@@ -19,6 +44,14 @@ class XnatpyRequestsMocker(Mocker):
     ):
         url = f"https://xnat.example.com/{url.lstrip('/')}"
         return super().request(method, url, **kwargs)
+
+    def create_object(self, obj=None):
+        if obj is None:
+            obj = CreatedObject(uri='/data/created/object')
+
+        patch('session')
+
+
 
 
 @fixture(scope='function')
@@ -70,6 +103,13 @@ def xnatpy_connection(mocker: MockerFixture,
         logger=logger,
         interface=requests_session,
     )
+
+    # Patch create object to avoid a lot of hassle
+    def create_object(uri, type_=None, fieldname=None, **kwargs):
+        return CreatedObject(uri, type_, fieldname, **kwargs)
+
+    xnat_session.create_object = create_object
+
     yield xnat_session
 
     # Close connection before the mocker gets cleaned
